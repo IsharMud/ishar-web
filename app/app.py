811fec497e6f77d5d94579827c0d5fa909a21a53
Eric OC <eric@ericoc.com>
2022-06-01 20:40:04 -0400

    python flask

---
+++ app/app.py
@@ -0,0 +1,8 @@
+import secrets
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html.j2', discord_link=secrets.discord_invite_link)
