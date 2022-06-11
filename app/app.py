6a7d1d06a6bb281207edf4e2ccdc2957cbbe9637
Ishar <admin@isharmud.com>
2022-06-11 17:14:15 +0000

    rewrite the help section entirely

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from flask import Flask, flash, make_response, redirect, render_template, reques
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
-
 # Errors (404)
 @app.errorhandler(404)
 def page_not_found(message):
@@ -29,20 +28,9 @@ def login():
 
 # /help
 @app.route('/help', methods=['GET'])
-@app.route('/help/<string:letter>')
-@app.route('/help/<string:letter>/')
-@app.route('/help/<string:letter>/<string:page>.html')
-def help(letter=None, page=None):
-    return render_template('help.html.j2', letter=letter, page=page)
-
-# Search help (POST /help) / WIP!!!
-@app.route('/help', methods=['POST'])
-def search_help(search_help=None):
-    if request.form['search_help'] and request.form['search_help'] != '':
-        search_help = request.form['search_help']
-        return help(letter=search_help[0], page=search_help)
-    else:
-        return help()
+@app.route('/help/<string:page>', methods=['GET'])
+def help(page=None):
+    return render_template('help.html.j2', page=page)
 
 # /clients
 @app.route('/clients')
@@ -86,7 +74,65 @@ def discord():
     discord_invite_link = 'https://discord.gg/VBmMXUpeve'
     return redirect(config.discord_invite_link, code=302)
 
-# /world
-@app.route('/world')
-def world():
-    return render_template('world.html.j2')
+# /areas
+@app.route('/areas', methods=['GET'])
+@app.route('/areas/<string:area>', methods=['GET'])
+def areas(area=None):
+
+    try:
+        areas = _get_help_area(area)
+        code = 200
+    except Exception as e:
+        areas = _get_help_area(None)
+        area = None
+        code = 404
+        print(f"Bad area? {e}")
+
+    return render_template('areas.html.j2', areas=areas, area=area), code
+
+
+def _get_help_area(area=None):
+
+    # Set "helptab" file name and open it
+    helptab_file = '/home/ishartest/ishar-mud/lib/Misc/helptab'
+    helptab_fh = open(helptab_file, 'r')
+
+    # Prepare an empty "areas" dictionary
+    areas = {}
+
+    # Do not keep lines by default
+    keep = False
+
+    # Loop through each line, finding and keeping chunks staring with "32 Area "
+    for line in helptab_fh:
+
+        stripped = line.strip()
+
+        # Stop line (#)
+        if keep == True and stripped == '#':
+            keep = False
+
+        # Do not include "other levels" info (%%)
+        if keep and stripped.startswith('%% '):
+            keep = False
+
+        # Append the current chunk to our areas dictionary, under the key of whatever started with "32 Area " last
+        if keep == True and not stripped.startswith('32 Area '):
+            areas[area_name] += line
+
+        # Start new dictionary keys of chunks at lines beginning with "32 Area "
+        if stripped.startswith('32 Area '):
+            keep = True
+            area_name = stripped.replace('32 Area ', '')
+            areas[area_name] = ''
+
+    # Close the "helptab" file
+    helptab_fh.close()
+
+    # Return either the single area, or a list of them
+    if area != None and areas[area]:
+        return areas[area]
+    elif areas != None and len(areas) > 0:
+        return areas
+    else:
+        return None
