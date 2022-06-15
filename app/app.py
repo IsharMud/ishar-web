a8941533502e9e9479b9f05e22243c013d35bbbc
Ishar <admin@isharmud.com>
2022-06-15 22:25:29 +0000

    stop try:ing so hard

--- app/app.py
+++ app/app.py
@@ -44,14 +44,14 @@ def internal_server_error(message):
 
 
 # Main welcome page/index (/)
-@app.route('/welcome')
-@app.route('/')
+@app.route('/welcome', methods=['GET'])
+@app.route('/', methods=['GET'])
 def welcome():
     return render_template('welcome.html.j2')
 
 
 # /history (formerly "Background" page)
-@app.route('/history')
+@app.route('/history', methods=['GET'])
 def history():
     return render_template('history.html.j2')
 
@@ -60,39 +60,25 @@ def history():
 @app.route('/login', methods=['POST'])
 def login():
 
-    try:
+    # Find the user by e-mail address from the log in form
+    user = models.Account.query.filter_by(email = request.form['email']).first()
 
-        # Find the user by e-mail address from the log in form
-        user = models.Account.query.filter_by(email = request.form['email']).first()
+    # If we find the user and match the password, it is a successful log in, so redirect to portal
+    if user != None and user.check_password(request.form['password']):
+        flash('You successfully logged in!', 'success')
+        login_user(user)
+        return redirect(url_for('portal'), code=302)
 
-        # If we find the user and match the password, it is a successful log in, so redirect to portal
-        if user != None and user.check_password(request.form['password']):
-            flash('You successfully logged in!', 'success')
-            login_user(user)
-            return redirect(url_for('portal'), code=302)
-
-        # Otherwise, there must have been invalid credentials?
-        else:
-            flash('Sorry, but please enter a valid e-mail address and password.', 'error')
-            return login_form()
-
-    except Exception as e:
-        logout_user()
-        print(f"Log in exception: {e}")
-        flash('Sorry, but there was an error logging in!', 'error')
+    # Otherwise, there must have been invalid credentials?
+    else:
+        flash('Sorry, but please enter a valid e-mail address and password.', 'error')
         return login_form()
 
 
 # Log in page (GET /login)
 @app.route('/login', methods=['GET'])
 def login_form():
-    try:
         logout_user()
-        return render_template('login_form.html.j2')
-    except Exception as e:
-        logout_user()
-        print(f"Log in form exception: {e}")
-        flash('Sorry, but there was an error with the log in form!', 'error')
         return render_template('login_form.html.j2')
 
 
@@ -102,33 +88,22 @@ def login_form():
 def portal():
 
     # Find the current season information and show the portal
-    try:
-        season = models.Season.query.filter_by(is_active = 1).first()
-        return render_template('portal.html.j2', season=season)
+    season = models.Season.query.filter_by(is_active = 1).first()
+    return render_template('portal.html.j2', season=season)
 
-    except Exception as e:
-        logout_user()
-        print(f"Portal exception: {e}")
-        flash('Sorry, but there was an error visiting the portal!', 'error')
-        return login_form()
 
 # /logout
 @app.route('/logout', methods=['GET'])
 @login_required
 def logout():
-    try:
-        logout_user()
-        flash('You have been logged out successfully! See you again next time!', 'success')
-        return login_form()
-    except Exception as e:
-        print(f"Logout exception: {e}")
-        flash('Sorry, but there was an error logging you out!', 'error')
-        return login_form()
+    logout_user()
+    flash('You have been logged out successfully! See you again next time!', 'success')
+    return login_form()
 
 
 # /clients or /mud_clients
-@app.route('/clients')
-@app.route('/mud_clients')
+@app.route('/clients', methods=['GET'])
+@app.route('/mud_clients', methods=['GET'])
 def mud_clients():
 
     mud_clients = {
@@ -159,14 +134,14 @@ def mud_clients():
 
 
 # Redirect /connect to mudslinger.net
-@app.route('/connect')
+@app.route('/connect', methods=['GET'])
 def connect():
     mudslinger_app_link = 'https://mudslinger.net/play/?host=isharmud.com&port=23'
     return redirect(mudslinger_app_link, code=302)
 
 
 # Redirect /discord to the invite link
-@app.route('/discord')
+@app.route('/discord', methods=['GET'])
 def discord():
     discord_invite_link = 'https://discord.gg/VBmMXUpeve'
     return redirect(discord_invite_link, code=302)
@@ -182,8 +157,8 @@ def get_started():
 
 
 # /support (or /donate)
-@app.route('/donate')
-@app.route('/support')
+@app.route('/donate', methods=['GET'])
+@app.route('/support', methods=['GET'])
 def support():
     return render_template('support.html.j2')
 
@@ -193,19 +168,18 @@ def support():
 @app.route('/areas', methods=['GET'])
 @app.route('/world/<string:area>', methods=['GET'])
 @app.route('/world', methods=['GET'])
-def world(area=None):
+def world(area=None, code=200):
 
     # Try to find an area based on user input
     try:
         areas = helptab._get_help_area(area)
-        code = 200
 
     # Otherwise, list all areas found in the game "helptab" file
     except Exception as e:
+        flash('Sorry, but please choose a valid area!', 'error')
         areas = helptab._get_help_area(None)
         area = None
         code = 404
-        print(f"Bad area? {e}")
 
     return render_template('world.html.j2', areas=areas, area=area), code
 
