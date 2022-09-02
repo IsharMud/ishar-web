1d2d1748f3875128cd07c2eaf33ef115a1f9bd08
Ishar <admin@isharmud.com>
2022-09-02 16:52:01 +0000

    commented out some unused classes and get_flag

--- app/models.py
+++ app/models.py
@@ -157,25 +157,25 @@ class AccountsUpgrade(db.Model):
 
 
 # Affect Flag database class (unused)
-class AffectFlag(db.Model):
-    __tablename__   = 'affect_flags'
+#class AffectFlag(db.Model):
+#    __tablename__   = 'affect_flags'
 
-    flag_id = Column(TINYINT(4), primary_key=True)
-    name    = Column(String(30), nullable=False, unique=True)
+#    flag_id = Column(TINYINT(4), primary_key=True)
+#    name    = Column(String(30), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<AffectFlag> "{self.name}" ({self.flag_id})'
+#    def __repr__(self):
+#        return f'<AffectFlag> "{self.name}" ({self.flag_id})'
 
 
 # Board database class (unused)
-class Board(db.Model):
-    __tablename__   = 'boards'
+#class Board(db.Model):
+#    __tablename__   = 'boards'
 
-    board_id    = Column(TINYINT(4), primary_key=True)
-    board_name  = Column(String(15), nullable=False)
+#    board_id    = Column(TINYINT(4), primary_key=True)
+#    board_name  = Column(String(15), nullable=False)
 
-    def __repr__(self):
-        return f'<Board> "{self.board_name}" ({self.board_id})'
+#    def __repr__(self):
+#        return f'<Board> "{self.board_name}" ({self.board_id})'
 
 
 # Challenge database class
@@ -200,25 +200,25 @@ class Challenge(db.Model):
 
 
 # Condition database class (unused)
-class Condition(db.Model):
-    __tablename__   = 'conditions'
+#class Condition(db.Model):
+#    __tablename__   = 'conditions'
 
-    condition_id    = Column(TINYINT(4), primary_key=True)
-    name            = Column(String(20), nullable=False, unique=True)
+#    condition_id    = Column(TINYINT(4), primary_key=True)
+#    name            = Column(String(20), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<PlayerCondition> "{self.name}" ({self.condition_id})'
+#    def __repr__(self):
+#        return f'<PlayerCondition> "{self.name}" ({self.condition_id})'
 
 
 # DisplayOption database class (unused)
-class DisplayOption(db.Model):
-    __tablename__   = 'display_options'
+#class DisplayOption(db.Model):
+#    __tablename__   = 'display_options'
 
-    display_id  = Column(TINYINT(4), primary_key=True)
-    name        = Column(String(20), nullable=False, unique=True)
+#    display_id  = Column(TINYINT(4), primary_key=True)
+#    name        = Column(String(20), nullable=False, unique=True)
 
-    def __repr__(self):
-        return f'<DisplayOption> "{self.name}" ({self.display_id})'
+#    def __repr__(self):
+#        return f'<DisplayOption> "{self.name}" ({self.display_id})'
 
 
 # News database class
@@ -273,8 +273,6 @@ class PlayerFlag(db.Model):
     flag_id = Column(INTEGER(11), primary_key=True)
     name    = Column(String(20), nullable=False, unique=True)
 
-    flag    = relationship('PlayersFlag', backref='flag')
-
     def __repr__(self):
         return f'<PlayerFlag> "{self.name}" ({self.flag_id})'
 
@@ -294,8 +292,6 @@ class PlayersFlag(db.Model):
                     ), nullable=False, index=True, primary_key=True
                 )
     value       = Column(INTEGER(11), nullable=False, server_default=FetchedValue())
-
-    player      = relationship('Player', backref='flags')
 
     def __repr__(self):
         return f'<PlayersFlag> "{self.flag.name}" ({self.flag_id}) @ <Player> "{self.player.name}" ({self.player_id}) : {self.value}'
@@ -404,44 +400,44 @@ class Season(db.Model):
 
 
 # Skill database class (unused)
-class Skill(db.Model):
-    __tablename__   = 'skills'
+#class Skill(db.Model):
+#    __tablename__   = 'skills'
 
-    skill_id        = Column(INTEGER(11), primary_key=True)
-    class_id        = Column(
-                        ForeignKey('classes.class_id',
-                            ondelete='CASCADE', onupdate='CASCADE'
-                        ), nullable=False, index=True
-                    )
-    max_value       = Column(INTEGER(11), nullable=False)
-    difficulty      = Column(INTEGER(11), nullable=False)
+#    skill_id        = Column(INTEGER(11), primary_key=True)
+#    class_id        = Column(
+#                        ForeignKey('classes.class_id',
+#                            ondelete='CASCADE', onupdate='CASCADE'
+#                        ), nullable=False, index=True
+#                    )
+#    max_value       = Column(INTEGER(11), nullable=False)
+#    difficulty      = Column(INTEGER(11), nullable=False)
 
 #    player_skills   = relationship('PlayerSkill', backref='skill')
 
 
-    def __repr__(self):
-        return f'<Skill> ID {self.skill_id} / Class ID {self.class_id} / Max: {self.max_value} / Difficulty: {self.difficulty}'
+#    def __repr__(self):
+#        return f'<Skill> ID {self.skill_id} / Class ID {self.class_id} / Max: {self.max_value} / Difficulty: {self.difficulty}'
 
 # Player Skill database class (unused)
-class PlayerSkill(db.Model):
-    __tablename__   = 'player_skills'
+#class PlayerSkill(db.Model):
+#    __tablename__   = 'player_skills'
 
-    skill_id        = Column(
-                        ForeignKey('skills.skill_id',
-                            ondelete='CASCADE', onupdate='CASCADE'
-                        ), nullable=False, index=True, primary_key=True
-                    )
-    player_id       = Column(
-                        ForeignKey('players.id',
-                            ondelete='CASCADE', onupdate='CASCADE'
-                        ), nullable=False, index=True, primary_key=True
-                    )
-    skill_level     = Column(TINYINT(11), nullable=False, server_default=FetchedValue())
+#    skill_id        = Column(
+#                        ForeignKey('skills.skill_id',
+#                            ondelete='CASCADE', onupdate='CASCADE'
+#                        ), nullable=False, index=True, primary_key=True
+#                    )
+#    player_id       = Column(
+#                        ForeignKey('players.id',
+#                            ondelete='CASCADE', onupdate='CASCADE'
+#                        ), nullable=False, index=True, primary_key=True
+#                    )
+#    skill_level     = Column(TINYINT(11), nullable=False, server_default=FetchedValue())
 
 #    player         = relationship('Player', backref='skills')
 
-    def __repr__(self):
-        return f'<PlayerSkill> {self.skill_id} @ <Player> "{self.player.name}" ({self.player_id}) / Skill Level: {self.skill_level}'
+#    def __repr__(self):
+#        return f'<PlayerSkill> {self.skill_id} @ <Player> "{self.player.name}" ({self.player_id}) / Skill Level: {self.skill_level}'
 
 
 # Player database class
@@ -536,6 +532,27 @@ class Player(db.Model):
     quests_completed        = Column(INTEGER(11), nullable=False, server_default=FetchedValue())
     challenges_completed    = Column(INTEGER(11), nullable=False, server_default=FetchedValue())
 
+    # Method to return boolean for a specific player flag, by its flag name
+    def get_flag(self, flag_name=None):
+
+        # Ensure a flag name string was passed, and find its ID
+        if flag_name and isinstance(flag_name, str):
+            player_flag     = PlayerFlag.query.filter_by(
+                                name    = flag_name
+                            ).first()
+
+            # Find the flag value for the player, and return True if the flag value is 1
+            if player_flag.flag_id and isinstance(player_flag.flag_id, int):
+                players_flag    = PlayersFlag.query.filter_by(
+                                    flag_id     = player_flag.flag_id,
+                                    player_id   = self.id
+                                ).first()
+                if players_flag.value == 1:
+                    return True
+
+        # Otherwise, return False
+        return False
+
     # Hybrid property returning boolean whether player is a God
     @hybrid_property
     def is_god(self):
@@ -553,9 +570,7 @@ class Player(db.Model):
     # Hybrid property returning boolean whether player is a survival ("PERM_DEATH") character
     @hybrid_property
     def is_survival(self):
-        if self.flags[45].flag.name == 'PERM_DEATH' and self.flags[45].value == 1:
-            return True
-        return False
+        return self.get_flag('PERM_DEATH')
 
     # Hybrid property to return player alignment
     @hybrid_property
--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
 # TODO
 
-- Fix `PlayersFlags` model so `player.flags[45]` can be referenced more like `player.flags['PERM_DEATH']`
 - Fancy sortable and filter-able tables for leader board
     * Maybe this? https://blog.miguelgrinberg.com/post/beautiful-interactive-tables-for-your-flask-templates
