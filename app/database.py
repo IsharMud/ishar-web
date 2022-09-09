1570fe3e7678e3e11850ebc55332dfa6956a4aca
Ishar <admin@isharmud.com>
2022-09-09 17:25:53 +0000

    what if this solved it all

--- app/database.py
+++ app/database.py
@@ -3,9 +3,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine      = create_engine(database_secret.sql_uri, echo=True)
+engine      = create_engine(database_secret.sql_uri, echo=True, pool_pre_ping=True)
 db_session  = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base        = declarative_base()
 Base.query  = db_session.query_property()
-
