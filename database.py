3733a82c980c05c30285366afc0f2ff0bff16d7a
Eric OC <eric@ericoc.com>
2022-09-12 14:47:55 -0400

    so we can disable echo in prod

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Database connection
-engine      = create_engine(database_secret.sql_uri, echo=True, pool_pre_ping=True)
+engine      = create_engine(database_secret.sql_uri, echo=database_secret.echo, pool_pre_ping=True)
 db_session  = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base        = declarative_base()
