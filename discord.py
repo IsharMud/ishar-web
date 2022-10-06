f9444333ad2cc339cc79d18e9e2269d08e516f7f
Eric OC <eric@ericoc.com>
2022-10-06 14:28:27 -0400

    clean up discord /season

--- discord.py
+++ discord.py
@@ -15,18 +15,10 @@ bot = interactions.Client(token=discord_secret.TOKEN, default_scope=discord_secr
 @bot.command()
 async def season(ctx: interactions.CommandContext):
     """Show the current Ishar MUD season information"""
-    current_season  = Season.query.filter_by(is_active = 1).first()
-    start_time = datetime.utcnow()
-    end_time = current_season.expiration_date
-    diff = relativedelta(start_time, end_time)
-    diff_str = ""
-    if diff.months > 0:
-        diff_str += f"{diff.months} months, "
-    diff_str += f"{diff.days} days , and {diff.hours} hours"
+    current_season  = Season.query.filter_by(is_active = 1).order_by(-Season.season_id).first()
     await ctx.send(
         f'It is currently Season {current_season.season_id}, ' \
-        f'which ends in {current_season.expires}, on ' \
-        f"{diff_str}!"
+        f'which ends in {current_season.expires}.'
     )
     db_session.close()
 
