89837c17f4dc7737851b3f0629b1f548da4fc58a
Eric OC <eric@ericoc.com>
2022-11-20 16:50:24 -0500

    much cleaner ifs

--- delta.py
+++ delta.py
@@ -1,28 +1,32 @@
 """Time delta handling"""
 
+
 def pluralish(timing=None):
     """Append "s" to stringified timedeltas, if not singular"""
     if timing['value'] != 1:
         timing['interval'] += 's'
-    return f"{timing['value']} {timing['interval']}"
+
+    # Round the number in the output string
+    return f"{round(timing['value'])} {timing['interval']}"
+
 
 def stringify(delta=None):
     """Stringify time deltas"""
-    delta_seconds   = delta.total_seconds()
-    if delta_seconds < 60:
-        ret = {'value': delta_seconds, 'interval': 'second'}
-    elif delta_seconds >= 60 and delta_seconds < 3600:
-        ret = {'value': int(delta_seconds / 60), 'interval': 'minute'}
-    elif delta_seconds >= 3600 and delta.days < 1:
-        ret = {'value': int(delta_seconds / 3600), 'interval': 'hour'}
-    elif delta.days >= 1 and delta.days < 7:
+
+    # Days require no math
+    if delta.days:
         ret = {'value': delta.days, 'interval': 'day'}
-    elif delta.days >= 7 and delta.days < 30:
-        ret = {'value': int(delta.days / 7), 'interval': 'week'}
-    elif delta.days >= 30 and delta.days < 365:
-        ret = {'value': int(delta.days / 30), 'interval': 'month'}
-    elif delta.days >= 365:
-        ret = {'value': int(delta.days / 365), 'interval': 'year'}
+
+    # Calculate hours
+    elif delta.seconds >= 3600:
+        ret = {'value': delta.seconds / 3600, 'interval': 'hour'}
+
+    # Calculate minutes
+    elif delta.seconds >= 60:
+        ret = {'value': delta.seconds / 60, 'interval': 'minute'}
+
+    # Seconds require no math
     else:
-        return str(delta)
+        ret = {'value': delta.seconds, 'interval': 'second'}
+
     return pluralish(timing=ret)
