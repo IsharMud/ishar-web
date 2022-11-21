d43acff45b7e388de37bc0928037f776df7a1a18
Eric OC <eric@ericoc.com>
2022-11-20 22:27:18 -0500

    pycharm suggestions

--- database.py
+++ database.py
@@ -4,20 +4,20 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import database_secret
 
-engine  = create_engine(
-            url             = database_secret.URL,
-            echo            = database_secret.ECHO,
-            pool_pre_ping   = True
+engine = create_engine(
+            url=database_secret.URL,
+            echo=database_secret.ECHO,
+            pool_pre_ping=True
         )
 
-db_session  = scoped_session(
+db_session = scoped_session(
                 sessionmaker(
-                    autocommit  = False,
-                    autoflush   = False,
-                    bind        = engine
+                    autocommit=False,
+                    autoflush=False,
+                    bind=engine
                 )
             )
 
-Base        = declarative_base()
-Base.query  = db_session.query_property()
-metadata    = Base.metadata
+Base = declarative_base()
+Base.query = db_session.query_property()
+metadata = Base.metadata
