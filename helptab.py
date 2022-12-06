ba6734c7bd6afcab73fd9da52b853b6f6465d2e3
Eric OC <eric@ericoc.com>
2022-12-06 18:49:08 -0500

    parse help topic body text

--- helptab.py
+++ helptab.py
@@ -1,4 +1,5 @@
 """Parse the MUD 'helptab' file"""
+import re
 from mud_secret import HELPTAB, IMM_LEVELS
 from sentry import sentry_sdk
 
@@ -70,7 +71,9 @@ def parse_help_chunk(help_chunk=None):
 
             # Topic body text is everything after the header and '*'
             help_topic['body_text'] = halves[1].replace('>', '&gt;').replace('<', '&lt;').replace('"', '&quot;')
-            help_topic['body_lines'] = help_topic['body_text'].split('\n')
+            for i in ['syntax', 'level', 'minimum', 'class', 'topic', 'save']:
+                help_topic[i] = parse_help_body(help_topic['body_text'], i)
+            help_topic['see_also'] = parse_help_see_also(help_topic['body_text'])
 
     # Return the help topic dictionary
     return help_topic
@@ -79,13 +82,13 @@ def parse_help_chunk(help_chunk=None):
 def parse_help_header(header=None):
     """Parse the header from a single topic chunk from the MUD 'helptab' file"""
 
-    # The 'level' of the help topic is a number alone, on the first line of the header
+    # The 'for_level' of the help topic is a number alone, on the first line of the header
     help_header = {}
     header_lines = header.split('\n')
-    help_header['level'] = int(header_lines[0].strip())
+    help_header['for_level'] = int(header_lines[0].strip())
 
     # Name the topic if mortals should be able reach it, and it starts with "32 "
-    if help_header['level'] < min(IMM_LEVELS) and header_lines[1].startswith('32 '):
+    if help_header['for_level'] < min(IMM_LEVELS) and header_lines[1].startswith('32 '):
         help_header['name'] = header_lines[1].replace('32 ', '').strip()
         help_header['aliases'] = []
 
@@ -102,6 +105,57 @@ def parse_help_header(header=None):
 
     # Return the help topic header
     return help_header
+
+
+def parse_help_body(body=None, search=None):
+    """Parse the body from a single topic chunk from the MUD 'helptab' file"""
+
+    # Split the body into lines and loop through each line
+    lines = body.split('\n')
+    for line in lines:
+
+        # Get the value after the colon (:) for any lines that matched the search
+        if re.search(r'^(\s*)(' + search + r')(\s*):(\s*)', line.strip().lower()):
+            value = line.split(':')
+            return value[-1].strip()
+
+    # No matches returns None
+    return None
+
+
+def parse_help_see_also(body=None):
+    """Parse the body from a single topic chunk from the MUD 'helptab' file,
+        looking for related topics"""
+
+    # Start an empty list of related help topics,
+    #   split the body into lines, and keep no lines by default
+    see_also = []
+    lines = body.split('\n')
+    keep = False
+
+    # Loop through each line of the topic chunk body
+    for line in lines:
+
+        # Keep anything after a phrase that seems like it indicates related topics:
+        #   such as "see also", "also see", "also see help on", "related" etc.
+        if re.search(r'^(\s*)(see also|also see|also see help on|related)(\s*):(\s*)', line.strip().lower()):
+            keep = True
+
+        if keep:
+            # Remove anything before the colon (:) and get each topic
+            rmbegin = line.split(':')
+            related_topics = rmbegin[-1].strip().split(',')
+
+            # Append each related topic to our list to return
+            for related_topic in related_topics:
+                if related_topic and related_topic.strip() != '':
+                    see_also.append(related_topic.strip())
+
+        # This only really works because "See also" is always the end of every help topic chunk...
+        # If the "See also:" stuff was in the middle of the help topic chunk, keep would remain True...
+
+    # Return the list of related topics
+    return see_also
 
 
 def search_help_topics(all_topics=None, search=None):
