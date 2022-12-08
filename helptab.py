fe52f0fe664ae852d9a23268340d5d6faf2b745d
Eric O'Callaghan <eric@ericoc.com>
2022-12-08 17:54:37 -0500

    keep text and html for help topics

--- helptab.py
+++ helptab.py
@@ -82,7 +82,7 @@ def parse_help_chunk(help_chunk=None):
 
         # Proceed if the help header is valid
         if help_header and 'name' in help_header:
-            help_content = halves[1].replace('>', '&gt;').replace('<', '&lt;').replace('"', '&quot;')
+            help_content = halves[1]
             help_topic = parse_help_content(content=help_content)
             help_topic['name'] = help_header['name']
             help_topic['aliases'] = help_header['aliases']
@@ -96,6 +96,7 @@ def parse_help_body(line=None):
         using regular expressions to find specific items (such as Class, Syntax, etc.)"""
 
     # Loop through each item regular expression, looking for matches on items
+    line = line.replace('>', '&gt;').replace('<', '&lt;').replace('"', '&quot;')
     for item_name, regex in regexes.items():
         find = regex.findall(line)
         if find:
@@ -144,49 +145,58 @@ def parse_help_content(content=None):
     # Loop through each line of the help chunk body text
     is_see_also = False
     help_topic = {}
+    help_topic['body_html'] = str()
     help_topic['body_text'] = str()
     help_topic['see_also'] = []
     for line in content.split('\n'):
 
+        # Split up, append, and try to link related "See Also" topics appropriately
         if see_also_regex.match(line):
+            help_topic['body_html'] += 'See Also: '
             help_topic['body_text'] += 'See Also: '
             is_see_also = True
 
         if is_see_also:
+            i = 0
             rmbegin = line.split(':')
             related_topics = rmbegin[-1].strip().split(',')
-
-            # Append each related topic to our list to return
-            i = 0
             num_related = len(related_topics)
+
+            # Loop through each related topic to link them
             for related_topic in related_topics:
                 i += 1
                 if related_topic and related_topic.strip() != '':
                     related_link = f'<a href="/help/{related_topic.strip()}">{related_topic.strip()}</a>'
-                    help_topic['body_text'] += related_link
+                    help_topic['body_html'] += related_link
+                    help_topic['body_text'] += related_topic.strip()
                     help_topic['see_also'].append(related_topic.strip())
+
+                    # Comma-separate related help topics
                     if i != num_related:
+                        help_topic['body_html'] += ', '
                         help_topic['body_text'] += ', '
                     else:
                         is_see_also = False
 
+        # Parse anything else as if it is the body
         else:
-            # Parse the body line
             parsed_line = parse_help_body(line=line)
 
-            # Append strings to the body text
+            # Append strings to the body html and body text, appropriately
             if isinstance(parsed_line, str):
-                help_topic['body_text'] += f'{line}\n'
+                help_topic['body_html'] += parsed_line + '\n'
+                help_topic['body_text'] += line + '\n'
 
             # Set dictionary values from regex matches
             if isinstance(parsed_line, dict):
                 for item_name, item_value in parsed_line.items():
                     help_topic[item_name] = item_value
 
-    # Replace "`help command'" in the body text with link to that help command
+    # Replace "`help command'" in the body html text with link to that help command
     cmd_pattern = r"`help ([\w| ]+)'"
-    help_topic['body_text'] = re.sub(cmd_pattern, r'`<a href="/help/\1">help \1</a>`', help_topic['body_text'], re.MULTILINE)
+    help_topic['body_html'] = re.sub(cmd_pattern, r'`<a href="/help/\1">help \1</a>`', help_topic['body_html'], re.MULTILINE)
 
+    # Return the help topic
     return help_topic
 
 
