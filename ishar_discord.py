8e8751c97a0ac81799ddddfb1625f13f94f0d53e
Eric O'Callaghan <eric@ericoc.com>
2023-04-11 17:37:17 -0400

    trying to fix discord

--- ishar_discord.py
+++ ishar_discord.py
@@ -13,7 +13,6 @@ from config import DISCORD
 from database import db_session
 from help.helptab import search_help_topics
 
-from models.account import Account
 from models.challenge import Challenge
 from models.globalevent import GlobalEvent
 from models.player import Player
