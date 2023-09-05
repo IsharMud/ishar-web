38e672a3629279ac6302adc6f64a34a89dab82aa
Eric O'Callaghan <eric@ericoc.com>
2023-09-04 22:51:34 -0400

    try to improve filtering

--- ishar/apps/leaders/views.py
+++ ishar/apps/leaders/views.py
@@ -4,29 +4,31 @@ from django.views.generic.list import ListView
 from ...apps.player.models import Player
 
 
+MINIMUM_IMMORTAL = min(settings.IMMORTAL_LEVELS)
+
+
 class LeadersView(ListView):
     """
     Leaders view.
     """
-    model = Player
-    template_name = "leaders.html.djt"
     context_object_name = "leader_players"
-
-    queryset = model.objects.filter(
-        true_level__lt=min(settings.IMMORTAL_LEVELS)
+    queryset = Player.objects.filter(
+        true_level__lt=MINIMUM_IMMORTAL
     ).order_by(
         "-remorts", "-total_renown", "-quests_completed",
         "-challenges_completed", "deaths"
     )
+
+    template_name = "leaders.html.djt"
 
 
 class ClassicLeadersView(LeadersView):
     """
     Classic players leaders view.
     """
-    model = Player
-    queryset = model.objects.filter(
-        true_level__lt=min(settings.IMMORTAL_LEVELS), game_type__exact=0
+    queryset = Player.objects.filter(
+        true_level__lt=MINIMUM_IMMORTAL,
+        game_type__exact=0
     ).order_by(
         "-remorts", "-total_renown", "-quests_completed",
         "-challenges_completed", "deaths"
@@ -37,9 +39,9 @@ class SurvivalLeadersView(LeadersView):
     """
     Survival players leaders view.
     """
-    model = Player
-    queryset = model.objects.filter(
-        true_level__lt=min(settings.IMMORTAL_LEVELS), game_type__exact=1
+    queryset = Player.objects.filter(
+        true_level__lt=MINIMUM_IMMORTAL,
+        game_type__exact=1
     ).order_by(
         "-remorts", "-total_renown", "-quests_completed",
         "-challenges_completed", "deaths"
