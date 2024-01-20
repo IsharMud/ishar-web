8dbf8055240771e682b4ab9be9a37472b2a8abc3
Eric O'Callaghan <eric@ericoc.com>
2024-01-20 01:01:55 -0500

    gunicorn log x-forwarded-for

--- gunicorn_configuration.example.py
+++ gunicorn_configuration.example.py
@@ -7,4 +7,5 @@ workers = cpu_count() * 2 + 1
 threads = 2
 keepalive = 300
 capture_output = True
+access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 accesslog = errorlog = "/var/www/isharmud.com/gunicorn.log"
