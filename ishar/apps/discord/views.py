beb74a5bac63d1389b340d58c76e2d12e96769d9
Eric O'Callaghan <eric@ericoc.com>
2024-01-30 22:55:43 -0500

    print public key

--- ishar/apps/discord/views.py
+++ ishar/apps/discord/views.py
@@ -20,16 +20,18 @@ class InteractionsView(View):
     def post(self, request, *args, **kwargs):
 
         body = request.body
+        discord_public_key = settings.DISCORD["PUBLIC_KEY"]
         sig = request.headers.get("X-Signature-Ed25519")
         ts = request.headers.get("X-Signature-Timestamp")
         if not sig or not ts:
             raise SuspiciousOperation("Bad Signature.")
 
-        vk = VerifyKey(bytes.fromhex(settings.DISCORD["PUBLIC_KEY"]))
-
+        print('discord_public_key:', discord_public_key)
         print("body:", body)
         print("sig:", sig)
         print("ts:", ts)
+
+        vk = VerifyKey(bytes.fromhex(discord_public_key))
         print("vk:", vk)
 
         try:
