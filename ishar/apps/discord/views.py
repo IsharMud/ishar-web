0282700fc856a659f16a875ebc44ec4215141e23
Eric O'Callaghan <eric@ericoc.com>
2024-01-31 22:41:43 -0500

    handle season command

--- ishar/apps/discord/views.py
+++ ishar/apps/discord/views.py
@@ -31,21 +31,28 @@ class InteractionsView(View):
         body = request.body.decode("utf-8")
 
         if not signature or not timestamp or not body:
-            msg = "Missing signature."
-            logging.error(msg)
-            return self.error(msg)
+            return self.error("Missing signature.")
 
         try:
             string = f"{timestamp}{body}".encode()
             verify_key.verify(string, bytes.fromhex(signature))
-            logging.info(f"{timestamp} KEY OK.")
-            logging.info(body)
-            logging.info(pformat(json.loads(body)))
-            logging.info(pformat(json.loads(body).get('type')))
-            logging.info(pformat(json.loads(body).get("data")))
-
-            return JsonResponse({"type": 1})
 
         except BadSignatureError as bad_sig:
             logging.exception(bad_sig)
             return self.error("Invalid signature.")
+
+        interaction_body = json.loads(body)
+        interaction_type = interaction_body.get("type")
+
+        if interaction_type == 1:
+            logging.info("PING: OK!")
+            return JsonResponse({"type": 1})
+
+        if interaction_type == 2:
+            interaction_data = interaction_body.get("data")
+            if interaction_data.get("name") == "season":
+                logging.info("Season command.")
+                return JsonResponse({
+                    "type": 4,
+                    "content": "Congrats on sending your command!"
+                })
