5425f7b3857458e420ab420561c115bff32b975b
Eric O'Callaghan <eric@ericoc.com>
2024-02-02 10:56:41 -0500

    split discord view to functions?

--- ishar/apps/discord/views.py
+++ ishar/apps/discord/views.py
@@ -19,6 +19,7 @@ class InteractionsView(View):
     """
     Interactions view.
     """
+    DISCORD = settings.DISCORD
     http_method_names = ("get", "post")
 
     @csrf_exempt
@@ -27,15 +28,78 @@ class InteractionsView(View):
 
     def error(self, message="Invalid request.", status=400) -> JsonResponse:
         logging.error("%s (%s)" % (message, status))
-        return JsonResponse(
-            data={
-                "error": message
-            },
-            status=status
-        )
+        return JsonResponse(data={"error": message}, status=status)
 
     def get(self, *args, **kwargs) -> JsonResponse:
         return self.error(message="Method not supported.", status=405)
+
+    def handle_command(self, interaction_data, request):
+
+        command_name = interaction_data.get("name")
+
+        # "deadhead" command - player with most deaths.
+        if command_name == "deadhead":
+            dead_head = Player.objects.filter(
+                true_level__lt=min(settings.IMMORTAL_LEVELS)[0],
+            ).order_by("-deaths").first()
+            return self.respond(
+                "%s :skull_crossbones: %i times!" % (
+                    dead_head.name,
+                    dead_head.deaths
+                )
+            )
+
+        # "events" command - any active events and when they expire.
+        elif command_name == "events":
+            global_events = GlobalEvent.objects.filter(
+                start_time__lt=now(),
+                end_time__gt=now()
+            ).all()
+
+            if global_events.count() > 0:
+                reply = "%i events:\n" % (global_events.count())
+                events = enumerate(global_events.all(), start=1)
+                for (num, event) in events:
+                    reply += "%i. %s - ends %s :alarm_clock: %s.\n" % (
+                        num,
+                        event.event_desc,
+                        timeuntil(event.end_time),
+                        event.end_time.strftime("%c %Z")
+                    )
+                return self.respond(reply)
+
+            return self.respond("Sorry - no events right now.")
+
+        # "faq" command to link frequently asked questions.
+        elif command_name == "faq":
+            return self.respond(
+                message="%s://%s%s" % (
+                    request.scheme,
+                    request.get_host(),
+                    reverse("faq")
+                )
+            )
+
+        # "mudtime" command to show server (UTC) time.
+        elif command_name == "mudtime":
+            message = now().strftime("%c :clock: %Z")
+
+        # "season" command - shows season number and expiration.
+        elif command_name == "season":
+            season = Season.objects.filter(is_active=1).first()
+            message = (
+                "Season %i :hourglass_flowing_sand: "
+                "ends %s :alarm_clock: %s." % (
+                    season.season_id,
+                    timeuntil(season.expiration_date),
+                    season.expiration_date.strftime("%c %Z")
+                )
+            )
+
+        else:
+            return self.error(message="Invalid command", status=400)
+
+        return self.respond(message)
 
     def respond(self, message=None, msg_type=4, status=200) -> JsonResponse:
         if message is not None:
@@ -54,86 +118,52 @@ class InteractionsView(View):
         return JsonResponse({"type": 1})
 
     def post(self, request, *args, **kwargs) -> JsonResponse:
-        verify_key = VerifyKey(bytes.fromhex(settings.DISCORD["PUBLIC_KEY"]))
-        signature = request.headers.get("X-Signature-Ed25519")
-        timestamp = request.headers.get("X-Signature-Timestamp")
-        body = request.body.decode("utf-8")
 
-        if not signature or not timestamp or not body:
-            return self.error("Missing signature.")
+        verify = self.validate(request)
+        if verify is None:
+            return self.error("Missing signature.", status=400)
+        if verify is False:
+            return self.error("Invalid signature.", status=400)
+        if verify is not True:
+            return self.error(
+                "Signature could not be verified.", status=500
+            )
 
-        try:
-            string = f"{timestamp}{body}".encode()
-            verify_key.verify(string, bytes.fromhex(signature))
-        except BadSignatureError as bad_sig:
-            logging.exception(bad_sig)
-            return self.error("Invalid signature.")
-
-        interaction_body = json.loads(body)
+        # Get the contents of the interaction and its type, if valid signature.
+        interaction_body = json.loads(request.body.decode("utf-8"))
         interaction_type = interaction_body.get("type")
 
         # Reply to ping with pong, for URL endpoint validation by Discord.
         if interaction_type == 1:
             return self.pong()
 
-        # Respond to different slash commands, based on the names.
+        # Reply to slash commands.
         if interaction_type == 2:
-            interaction_data = interaction_body.get("data")
-            command_name = interaction_data.get("name")
+            return self.handle_command(
+                request=request,
+                interaction_data=interaction_body.get("data"),
+            )
 
-            # "Deadhead" command - player with most deaths.
-            if command_name == "deadhead":
-                dead_head = Player.objects.filter(
-                    true_level__lt=min(settings.IMMORTAL_LEVELS)[0],
-                ).order_by("-deaths").first()
-                return self.respond(
-                    "%s :skull_crossbones: %i times!" % (
-                        dead_head.name,
-                        dead_head.deaths
-                    )
-                )
+        return self.error("Invalid command.", status=400)
 
-            # "Events" command - any active events and when they expire.
-            if command_name == "events":
-                global_events = GlobalEvent.objects.filter(
-                    start_time__lt=now(),
-                    end_time__gt=now()
-                ).all()
+    def validate(self, request):
+        """
+        Verify signature via headers of incoming Discord HTTPS requests.
+        """
+        verify_key = VerifyKey(bytes.fromhex(self.DISCORD["PUBLIC_KEY"]))
+        signature = request.headers.get("X-Signature-Ed25519")
+        timestamp = request.headers.get("X-Signature-Timestamp")
+        body = request.body.decode("utf-8")
 
-                if global_events.count() > 0:
-                    reply = "%i events:\n" % (global_events.count())
-                    events = enumerate(global_events.all(), start=1)
-                    for (num, event) in events:
-                        reply += "%i. %s - ends %s :alarm_clock: %s.\n" % (
-                            num,
-                            event.event_desc,
-                            timeuntil(event.end_time),
-                            event.end_time.strftime("%c %Z")
-                        )
-                    return self.respond(reply)
+        if not signature or not timestamp or not body:
+            return None
 
-                return self.respond("Sorry - no events right now.")
+        try:
+            string = f"{timestamp}{body}".encode()
+            if verify_key.verify(string, bytes.fromhex(signature)):
+                return True
 
-            # "faq" command to link frequently asked questions.
-            if command_name == "faq":
-                return self.respond(
-                    message="%s://%s%s" % (
-                        request.scheme, request.get_host(), reverse("faq")
-                    )
-                )
+        except (BadSignatureError, ValueError) as bad_sig:
+            logging.exception(bad_sig)
 
-            # "mudtime" command to show server (UTC) time.
-            if command_name == "mudtime":
-                return self.respond(now().strftime("%c :clock: %Z"))
-
-            # "Season" command - shows season number and expiration.
-            if command_name == "season":
-                season = Season.objects.filter(is_active=1).first()
-                return self.respond(
-                    "Season %i :hourglass_flowing_sand: "
-                    "ends %s :alarm_clock: %s." % (
-                        season.season_id,
-                        timeuntil(season.expiration_date),
-                        season.expiration_date.strftime("%c %Z")
-                    )
-                )
+        return False
