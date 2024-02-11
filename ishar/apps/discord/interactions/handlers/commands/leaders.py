97cecf8d16732323958c61923dbac590f26a08be
Eric O'Callaghan <eric@ericoc.com>
2024-02-11 13:09:23 -0500

    no expansion on leader link from discord

--- ishar/apps/discord/interactions/handlers/commands/leaders.py
+++ ishar/apps/discord/interactions/handlers/commands/leaders.py
@@ -3,4 +3,6 @@ from django.urls import reverse
 
 def leaders(request):
     """Link to leaders page."""
-    return "%s://%s%s" % (request.scheme, request.get_host(), reverse("leaders"))
+    return "<%s://%s%s>" % (
+        request.scheme, request.get_host(), reverse("leaders")
+    )
