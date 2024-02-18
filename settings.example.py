db78121f3c4ffb6633224d3baa560bce0486dfe5
Eric O'Callaghan <eric@ericoc.com>
2024-02-17 19:05:28 -0500

    switch ui builder off

--- settings.example.py
+++ settings.example.py
@@ -467,7 +467,7 @@ JAZZMIN_SETTINGS = {
     # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
     "use_google_fonts_cdn": True,
     # Whether to show the UI customizer on the sidebar
-    "show_ui_builder": True,
+    "show_ui_builder": False,
 
     ###############
     # Change view #
