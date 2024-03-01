7d7e459e2ac434590905b59ab2796327037a0ae2
Eric O'Callaghan <eric@ericoc.com>
2024-02-29 21:09:42 -0500

    cant have it both ways

--- ishar/apps/core/admin/__init__.py
+++ ishar/apps/core/admin/__init__.py
@@ -1,2 +1,15 @@
+from django.conf import settings
+
+from django.contrib import admin
+from django.contrib.auth.admin import Group
+
 from .affect_flag import AffectFlagAdmin
 from .player_flag import PlayerFlagAdmin
+
+
+# Set header and title text for /admin/
+admin.site.site_header = admin.site.site_title = settings.WEBSITE_TITLE
+admin.site.index_title = "Administration"
+
+# Disable "groups" in /admin/
+admin.site.unregister(Group)
