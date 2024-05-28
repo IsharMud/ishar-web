b8eee3a2f0eff62933f57f7066eedbce7a102897
Eric O'Callaghan <eric@ericoc.com>
2024-05-28 18:31:01 -0400

    spacing

--- ishar/apps/discord/interactions/handlers/commands/leaders.py
+++ ishar/apps/discord/interactions/handlers/commands/leaders.py
@@ -4,5 +4,7 @@ from django.urls import reverse
 def leaders(request):
     """Link to leaders page."""
     return ":trophy: <%s://%s%s>" % (
-        request.scheme, request.get_host(), reverse("leaders")
+        request.scheme,
+        request.get_host(),
+        reverse("leaders")
     )
