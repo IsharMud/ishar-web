ad1688f88fd7f4de7ed7eca88c47b911a16d1e04
Eric O'Callaghan <eric@ericoc.com>
2024-06-03 18:30:20 -0400

    inherit ImmortalAdmin has permission methods from PlayerAdmin

--- ishar/apps/players/admin/immortal.py
+++ ishar/apps/players/admin/immortal.py
@@ -11,19 +11,6 @@ class ImmortalAdmin(PlayerAdmin):
     list_display = ("name", "get_account_link", "player_type", "player_level")
     model = Immortal
 
-    def has_add_permission(self, request) -> bool:
-        return False
-
     def has_change_permission(self, request, obj=None) -> bool:
+        """Disable changing immortals."""
         return False
-
-    def has_delete_permission(self, request, obj=None) -> bool:
-        return False
-
-    def has_module_permission(self, request) -> bool:
-        if request.user and not request.user.is_anonymous:
-            return request.user.is_eternal()
-        return False
-
-    def has_view_permission(self, request, obj=None) -> bool:
-        return self.has_module_permission(request=request)
