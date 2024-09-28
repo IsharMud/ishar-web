31ca9a3f5cf0ad45a95d9bb1462931125eadce87
Eric O'Callaghan <eric@ericoc.com>
2024-09-28 14:59:17 -0400

    link leaders game type pages for discord

--- apps/discord/interactions/handlers/commands/leader.py
+++ apps/discord/interactions/handlers/commands/leader.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.urls import reverse
+
 from apps.leaders.models.leader import Leader
 from apps.players.models.game_type import GameType
 
@@ -6,14 +8,19 @@ from apps.players.models.game_type import GameType
 def leader(request, interaction=None):
 
     lead_label = settings.WEBSITE_TITLE
+    lead_url = f'{reverse("leaders")}#leaders'
     qs = Leader.objects
 
     interaction_options = interaction.get("options")
     if interaction_options:
-        find_game_type = interaction_options[0].get("value")
+        find_game_type = interaction_options[0]["value"]
         game_type = GameType._value2member_map_[int(find_game_type)]
         qs = qs.filter(game_type__exact=game_type.value)
         lead_label = game_type.label
+        lead_url = f'{reverse(lead_label.lower())}#{lead_label.lower()}'
 
     lead_player = qs.first()
-    return f':trophy: {lead_player.name} is the current {lead_label} leader!'
+    return (
+        f':trophy: {lead_player.name} is the current {lead_label} leader!'
+        f'<{request.scheme}://{request.get_host()}{lead_url}>'
+    )
