194bd9ae26a33c972dfb615ea63f75acbf71521f
Eric O'Callaghan <eric@ericoc.com>
2024-09-27 20:15:37 -0400

    update leader logic

--- apps/discord/interactions/handlers/commands/leader.py
+++ apps/discord/interactions/handlers/commands/leader.py
@@ -7,10 +7,18 @@ def leader(request, interaction=None):
     pprint(interaction["options"])
     find_game_type = interaction["options"][0].get("value")
     qs = Leader.objects
+    game_type_name = settings.WEBSITE_TITLE
     if find_game_type:
+        game_type_name = GameType._value2member_map_[find_game_type].label
         qs.filter(game_type__exact=find_game_type)
-    lead_player = qs.first()
+    lead_player = qs.order_by(
+        "-remorts",
+        "-statistics__total_renown",
+        "-statistics__total_challenges",
+        "-statistics__total_quests",
+        "statistics__total_deaths",
+        "-common__level"
+    ).first()
     return (
-        f':trophy: {lead_player.name} is the current'
-        f' {find_game_type or settings.WEBSITE_TITLE} leader!'
+        f':trophy: {lead_player.name} is the current {game_type_name} leader!'
     )
