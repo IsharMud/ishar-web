27a2a30ed3b6101280324956c605a720ca40e0d2
Eric O'Callaghan <eric@ericoc.com>
2024-10-03 22:27:50 -0400

    /challenges link for #43

--- apps/discord/interactions/handlers/commands/challenges.py
+++ apps/discord/interactions/handlers/commands/challenges.py
@@ -5,6 +5,6 @@ def challenges(request):
     # Link to challenges page.
     return (
         f':crossed_swords: [Challenges]'
-        f'<{request.scheme}://{request.get_host()}'
-        f'{reverse("challenges")}#challenges>'
+        f'(<{request.scheme}://{request.get_host()}'
+        f'{reverse("challenges")}#challenges>)'
     )
