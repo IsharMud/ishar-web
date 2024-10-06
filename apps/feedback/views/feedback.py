c4d44a57821d17cf29eadbbce5c23f1333fea08b
Eric O'Callaghan <eric@ericoc.com>
2024-10-06 13:26:19 -0400

    3 per page for feedback

--- apps/feedback/views/feedback.py
+++ apps/feedback/views/feedback.py
@@ -13,6 +13,7 @@ class FeedbackView(LoginRequiredMixin, NeverCacheMixin, ListView):
     context_object_name = "submissions"
     http_method_names = ("get",)
     model = FeedbackSubmission
+    paginate_by = 3
     queryset = model.objects.exclude(
         submission_type__exact=FeedbackSubmissionType.COMPLETE
     )
