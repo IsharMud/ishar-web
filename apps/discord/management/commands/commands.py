b4ebe0a721411e7e88a3ef355f41242feae5a0f5
Eric O'Callaghan <eric@ericoc.com>
2024-11-07 15:47:48 -0500

    build discord commands with a .yaml file instead of a big inline blob

--- apps/discord/management/commands/commands.py
+++ apps/discord/management/commands/commands.py
@@ -1,9 +1,9 @@
 import requests
+import yaml
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from pathlib import Path
 from pprint import pformat
-
-from apps.players.models.game_type import GameType
 
 
 class Command(BaseCommand):
@@ -45,117 +45,12 @@ class Command(BaseCommand):
             )
         )
 
-        all_commands = [
-            {
-                "type": 1,
-                "name": "challenges",
-                "description": "Link to the list of MUD challenges.",
-            },
-            {
-                "type": 1,
-                "name": "cycle",
-                "description": "Show when challenges will cycle next.",
-            },
-            {
-                "type": 1,
-                "name": "deadhead",
-                "description": "Show the player with the most deaths.",
-            },
-            {
-                "type": 1,
-                "name": "events",
-                "description": "Show any active events.",
-            },
-            {
-                "type": 1,
-                "name": "faq",
-                "description": "Link to frequently asked questions (FAQs).",
-            },
-            {
-                "type": 1,
-                "name": "feedback",
-                "description":
-                    "Link to feedback page to send bug report,"
-                    " idea, or suggestion.",
-            },
-            {
-                "name": "leader",
-                "type": 1,
-                "description": "List the leading player, by game type.",
-                "options": [{
-                    "name": "type",
-                    "description": "Game type to find the leader of.",
-                    "type": 3,
-                    "required": False,
-                    "choices": [
-                        {
-                            "name": "Classic",
-                            "value": str(GameType.CLASSIC.value)
-                        },
-                        {
-                            "name": "Hardcore",
-                            "value": str(GameType.HARDCORE.value)
-                        },
-                        {
-                            "name": "Survival",
-                            "value": str(GameType.SURVIVAL.value)
-                        }
-                    ]
-                }]
-            },
-            {
-                "type": 1,
-                "name": "leaders",
-                "description": "Link to the list of leading MUD players.",
-            },
-            {
-                "type": 1,
-                "name": "mudhelp",
-                "description": "Search MUD help for a topic.",
-                "options": [
-                    {
-                        "name": "topic",
-                        "description": "Name of the help topic to search for.",
-                        "type": 3,
-                        "required": True,
-                    }
-                ],
-            },
-            {
-                "type": 1,
-                "name": "mudtime",
-                "description": "Show current server time.",
-            },
-            {
-                "type": 1,
-                "name": "runtime",
-                "description": "Show current server runtime.",
-            },
-            {
-                "type": 1,
-                "name": "season",
-                "description": "Show the current season.",
-            },
-            {
-                "type": 1,
-                "name": "spell",
-                "description": "Search MUD help for a spell.",
-                "options": [
-                    {
-                        "name": "spell",
-                        "description": "Name of the spell to search for.",
-                        "type": 3,
-                        "required": True,
-                    }
-                ],
-            },
-            {
-                "type": 1,
-                "name": "upgrades",
-                "description":
-                    "Link to available remort upgrades and their renown costs."
-            },
-        ]
+        with open(
+            file=Path(Path(__file__).parent, "commands.yaml"),
+            mode="r",
+            encoding="utf-8"
+        ) as cmd_yml:
+            all_commands = yaml.safe_load(cmd_yml)
 
         reg = requests.put(
             url=discord_url,
