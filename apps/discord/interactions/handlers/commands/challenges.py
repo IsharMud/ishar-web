4a75278b5ebd62f101ef0fd1bcd2440a9281dcd7
Eric O'Callaghan <eric@ericoc.com>
2025-03-12 23:57:27 -0400

    cleaner return of discord challenges

--- apps/discord/interactions/handlers/commands/challenges.py
+++ apps/discord/interactions/handlers/commands/challenges.py
@@ -5,22 +5,22 @@ from apps.challenges.models import Challenge
 
 def challenges(request, interaction=None):
     # Link to challenges, with counts of active, complete, and incomplete.
-    challenges_url = f'{reverse("challenges")}#challenges'
+    icon = ":crossed_swords:"
 
     # TODO: use interaction.
     print("Discord Challenges interaction:", interaction)
 
-    try:
-        qs = Challenge.objects.filter(is_active__exact=1)
-        num_active = qs.count()
-        num_complete = qs.exclude(winner_desc__exact="").count()
-        num_incomplete = qs.filter(winner_desc__exact="").count()
-    except Challenge.DoesNotExist:
-        num_active = num_complete = num_incomplete = 0
+    qs = Challenge.objects.filter(is_active__exact=1)
+    num_complete = qs.exclude(winner_desc__exact="").count()
+    num_incomplete = qs.filter(winner_desc__exact="").count()
 
-    return (
-        f":crossed_swords: [Challenges]"
-        f"(<{request.scheme}://{request.get_host()}{challenges_url}>) -"
-        f" {num_active} active - {num_complete} complete /"
-        f" {num_incomplete} incomplete."
-    )
+    anchor = "challenges"
+    base_url = f"{request.scheme}://{request.get_host()}"
+    challenges_url = (f"[Challenges]"
+                      f"(<{base_url}{reverse(anchor)}{anchor}>)")
+    complete_url = (f"[{num_complete} complete]"
+                    f"(<{base_url}{reverse('complete')}{anchor}>)")
+    incomplete_url = (f"[{num_incomplete} incomplete]"
+                      f"(<{base_url}{reverse('incomplete')}{anchor}>)")
+
+    return f"{icon} {challenges_url} / {complete_url} - {incomplete_url}."
