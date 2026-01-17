7c4fa770daa10f020edd9afe8060cb7a22778299
Eric O'Callaghan <eric@ericoc.com>
2026-01-17 01:02:20 -0500

    fix racesadmin deathloads

--- apps/races/models/deathload.py
+++ apps/races/models/deathload.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
 from .race import Race
+from ...classes.models.cls import Class
 
 
 class RaceDeathload(models.Model):
@@ -11,7 +13,7 @@ class RaceDeathload(models.Model):
         db_column="racial_deathload_id",
         help_text="Primary identification number of the race deathload.",
         primary_key=True,
-        verbose_name="Race Deathload ID",
+        verbose_name=_("Race Deathload ID"),
     )
     race = models.ForeignKey(
         blank=False,
@@ -22,13 +24,13 @@ class RaceDeathload(models.Model):
         help_text="Race related to the deathload.",
         related_name="deathloads",
         related_query_name="deathload",
-        verbose_name="Race",
+        verbose_name=_("Race"),
     )
     vnum = models.PositiveIntegerField(
         blank=False,
         help_text="VNUM of the race deathload.",
         null=False,
-        verbose_name="VNUM",
+        verbose_name=_("VNUM"),
     )
     percent_chance = models.PositiveIntegerField(
         blank=False,
@@ -39,7 +41,7 @@ class RaceDeathload(models.Model):
             MinValueValidator(limit_value=0),
             MaxValueValidator(limit_value=100),
         ),
-        verbose_name="Percent Chance",
+        verbose_name=_("Percent Chance"),
     )
     min_level = models.PositiveIntegerField(
         blank=False,
@@ -50,7 +52,7 @@ class RaceDeathload(models.Model):
             MinValueValidator(limit_value=1),
             MaxValueValidator(limit_value=30),
         ),
-        verbose_name="Minimum Level",
+        verbose_name=_("Minimum Level"),
     )
     max_level = models.PositiveIntegerField(
         blank=True,
@@ -61,14 +63,33 @@ class RaceDeathload(models.Model):
             MinValueValidator(limit_value=1),
             MaxValueValidator(limit_value=30),
         ),
-        verbose_name="Maximum Level",
+        verbose_name=_("Maximum Level"),
     )
-    max_load = models.PositiveIntegerField(
+
+    min_quantity = models.PositiveIntegerField(
         blank=True,
         default=1,
-        help_text="Maximum load number of the race deathload.",
+        help_text=_("Minimum number of this item to load"),
+        null=False,
+        verbose_name=_("Minimum Quantity"),
+    )
+    max_quantity = models.PositiveIntegerField(
+        blank=True,
+        help_text=_("Maximum number of this item to load"),
         null=True,
-        verbose_name="Maximum Load",
+        verbose_name=_("Maximum Quantity"),
+    )
+    class_restrict = models.ForeignKey(
+        blank=True,
+        db_column="class_restrict",
+        default=4,
+        to=Class,
+        on_delete=models.CASCADE,
+        null=True,
+        help_text=_("Class which the deathload is restricted to."),
+        related_name="deathloads",
+        related_query_name="deathload",
+        verbose_name=_("Class Restrict"),
     )
 
     class Meta:
@@ -76,11 +97,11 @@ class RaceDeathload(models.Model):
         default_related_name = "deathload"
         managed = False
         ordering = ("race", "vnum", "percent_chance", "min_level")
-        verbose_name = "Deathload"
-        verbose_name_plural = "Deathloads"
+        verbose_name = _("Deathload")
+        verbose_name_plural = _("Deathloads")
 
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}: {self.__str__()} ({self.pk})"
 
     def __str__(self) -> str:
-        return f"{self.vnum} @ {self.race}"
+        return f"{self.vnum} @ {self.race} : {self.class_restrict}"
