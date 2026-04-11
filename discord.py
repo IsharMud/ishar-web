244fc3c8d53f86824191d7b1e0ac57b98ecd8ddb
Eric O'Callaghan <eric@ericoc.com>
2026-04-11 19:25:52 -0400

    see if this discord stuff works!

--- discord.py
+++ discord.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 
+from django import setup
 from django.conf import settings
 from interactions import Client, Intents, listen, slash_command, SlashContext
-
-from apps.leaders.models import Leader
 
 
 """
 Set up.
 """
+setup()
 bot = Client(intents=Intents.DEFAULT)
+
+from apps.leaders.models import Leader
+from apps.leaders.models import Player
+
 
 @listen()
 async def on_ready():
     """Say so when the bot is ready!"""
-    print("Ready!")
-    print(f"bot:{bot}")
+    print(f"Ready! ({bot})")
 
 
 #@listen()
@@ -29,8 +32,12 @@ Commands.
 
 @slash_command(name="deadhead", description="Which player has died most?")
 async def deadhead_function(ctx: SlashContext):
-    """List which player has the highest number of deaths."""
-    await ctx.send("The player who has died most is __TODO__. Sorry.")
+    """List which player has the highest number of total deaths."""
+    death = await Player.objects.order_by("-statistics__total_deaths").afirst()
+    await ctx.send(
+        f"_{death.player_title}_ has died :skull_crossbones: "
+        f" __{death.statistics.total_deaths}__ times!"
+    )
 
 
 @slash_command(name="faq", description="Frequently Asked Questions")
@@ -42,10 +49,10 @@ async def faq_function(ctx: SlashContext):
 @slash_command(name="leader", description="Who is the leading player?")
 async def leader_function(ctx: SlashContext):
     """Leader."""
-    leader = Leader.objects.first()
+    leader = await Leader.objects.afirst()
     await ctx.send(
         f"_{leader.player_title}_ is the leading player!"
-        f" <https://isharmud.com/leaders/#leaders>"
+        f" :trophy: <https://isharmud.com/leaders/#leaders>"
     )
 
 
@@ -55,7 +62,7 @@ async def leader_function(ctx: SlashContext):
 )
 async def start_function(ctx: SlashContext):
     """Link the website getting started guide."""
-    await ctx.send("https://isharmud.com/start/#start")
+    await ctx.send("https://isharmud.com/start/#start :rocket:")
 
 
 """
