2570905e1990981e2c7fe8218060776b7eac9a7b
Eric O'Callaghan <eric@ericoc.com>
2026-04-18 13:15:39 -0400

    this didn't work

--- apps/seasons/utils/current.py
+++ apps/seasons/utils/current.py
@@ -5,6 +5,6 @@ def get_current_season():
     # Get the current, latest, active Ishar MUD season.
     return Season.objects.latest()
 
-async def aget_current_season():
+def aget_current_season():
     # Asynchronously get the current, latest, active Ishar MUD season.
     return Season.objects.alatest()
